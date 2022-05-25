d6043a5721ab51ebb7a5625948f9c6f11dd1d897
lijiaxin <1003914407@qq.com>
2022-05-25 18:05:18 +0800

    实现 effect & reactive & 依赖收集 & 触发依赖

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,4 +1,12 @@
 /*
+ * @Author: lijiaxin 1003914407@qq.com
+ * @Date: 2022-05-24 20:01:27
+ * @LastEditors: lijiaxin 1003914407@qq.com
+ * @LastEditTime: 2022-05-25 18:00:34
+ * @FilePath: \min-vue\src\reactivity\effect.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
     1.首先effect是一个函数接收一个函数执行里面的代码
     2.后面如果被代理的数据更新effect接收的函数要再次执行所以需要一个容器接收这个函数
     3.收集依赖
@@ -17,7 +25,7 @@ class ReactiveEffect{
     }
 }
 
-let targetMap=new Map()  //一个接收所有触发过get的被proxy代理过的变量
+let targetMap=new Map()  //一个接收所有触发过get的被proxy代理过的变量的集合
 export function collect(target,key) {
     let depMap=targetMap.get(target)  //拿出在targetMap的被proxy代理过的变量的集合
     if(!depMap){ //如果 targetMap没有这个被proxy代理过的变量集合那就创建一个新的集合放进去
